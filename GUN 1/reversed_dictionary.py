4658980f8018f308113fb3f35db41797a0a38f98
omerbselvi <omerbugra97@hotmail.com>
2018-07-21 10:11:01 +0300

    add reversed_dictioary

---
+++ GUN 1/reversed_dictionary.py
@@ -0,0 +1,7 @@
+#usr/bin/env python
+sozluk ={'elma': 'apple', 'muz': 'banana', 'uzum': 'grapes', 'havuc': 'carrot'}
+print(sozluk)
+reverse_sozluk = {i:j for j,i in sozluk.items()}
+
+print(reverse_sozluk)
+print(reverse_sozluk["apple"])
